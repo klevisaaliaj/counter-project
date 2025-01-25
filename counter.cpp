04b60fc850b89ef65394ddd33c5709d799f6836f
Klevisa Aliaj <klevisaaliaj14@gmail.com>
2025-01-25 19:10:05 +0100

    logic

--- counter.cpp
+++ counter.cpp
@@ -1,4 +1,12 @@
 #include <iostream>
 int main(){
-    //hello
+    //create a counter app
+    //TODO: 1- the counter value that changes when u add or remove
+    //2- the formula to increment or subtract
+    //3- buttons one to increase the counter value and the other to decrease the counter value
+
+    // we have the counter value and we want to add one to it, this will be done by executing the formula to increment, how and when will this formula/function be executed?
+    //how? find a way to make the computer read keyboard input, if it reads arrow up, execute increment formula else if it reads arrow down execute decremet formula/function
+     
+
 }
